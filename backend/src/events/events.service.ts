ba8e1088dcd3a350ca7523e60478254da229a8bb
Joshua Wood <josh.nz@gmail.com>
2025-08-23 15:38:06 +1200

    Include available tickets when listing an event.

--- backend/src/events/events.service.ts
+++ backend/src/events/events.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Event } from './event.entity';
 import { CreateEventDto } from './event.new.dto';
 
@@ -10,14 +10,23 @@ export class EventsService {
   constructor(
     @InjectRepository(Event)
     private eventsRepository: Repository<Event>,
+    private dataSource: DataSource
   ) {}
 
   findAll(): Promise<Event[]> {
     return this.eventsRepository.find();
   }
 
-  findOne(id: string): Promise<Event | null> {
-    return this.eventsRepository.findOneBy({ id });
+  findOne(id: string) {
+    return this.dataSource.query(
+      `select e.name, e.date, count(e.id) as tickets_left
+       from events e
+       join tickets t on t.event_id = e.id
+       where e.id = $1
+        and (t.ticket_status_id = 1 or (t.ticket_status_id = 2 and t.reservation_expires_at < now()))
+       group by e.id, e.name, e.date;
+      `,
+      [id]);
   }
 
   async create(dto: CreateEventDto): Promise<string> {
